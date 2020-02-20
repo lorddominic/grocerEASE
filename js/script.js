6f8967634b5f468bc7d97644ee9faaf8c7fe4734
Clayton Dean <clay_holman8@yahoo.com>
2020-02-20 14:19:48 -0700

    has logo, targets API data-id, fixed column issue, rounded images, misc CSS

--- js/script.js
+++ js/script.js
@@ -7,20 +7,22 @@ let searchIcon = $('#searchButton')
 let $div = ('<div>')
 var ing = []
 var meas = []
+var recipes = []
 
-$('.multiple-items').on('click', function(e) {
+$('.multiple-items').on('click', function (e) {
 
-    
-    alert(this.attr())
+    var target = $(event.target).attr("data-id")
+    recipes.push(target);
+    console.log(recipes);
 })
 
 $("#recipe-form").on("submit", function (event) {
     event.preventDefault();
     console.log("search enter hit")
-   
+
     let ingredient = $("#search-input").val().trim();
     console.log(ingredient);
-    
+
 
     ingredientSearch(ingredient);
 
@@ -29,10 +31,11 @@ $("#recipe-form").on("submit", function (event) {
 
 searchIcon.on('click', function () {
     let ingredient = $('.uk-search-input').val().trim();
-   
+
     ingredientSearch(ingredient);
 
 });
+
 
 
 function ingredientSearch(ingredient) {
@@ -53,40 +56,40 @@ function ingredientSearch(ingredient) {
 
         for (let i = 0; i < 5; i++) {
 
-            
-           let meal = response.meals[i].strMeal
-           let mealThumb = response.meals[i].strMealThumb
-           let idMeal = response.meals[i].idMeal
-           let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal)
-    
+
+            let meal = response.meals[i].strMeal
+            let mealThumb = response.meals[i].strMealThumb
+            let idMeal = response.meals[i].idMeal
+            let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal)
+
             var sliderItem = $('<div>').append(imageCard)
-           $('.multiple-items').append(sliderItem)
-          
+            $('.multiple-items').append(sliderItem)
 
-           
-           
 
-           queryUrl2 = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
-           console.log(queryUrl2)
+
+
+
+            queryUrl2 = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
+            console.log(queryUrl2)
 
         }
-    
+
         $.ajax({
             method: "GET",
             url: queryUrl2
         }).then(function (response2) {
-                console.log(response2);
-               for(let i = 1; i < 20; i++){
-               if(response2.meals[0]['strIngredient' + i] != null){
-                ing.push(response2.meals[0]['strIngredient' + i])
-                meas.push(response2.meals[0]['strMeasure' + i])
-                console.log(ing)
-                console.log(meas)
-               }
-                
-               }
-               
-            
+            console.log(response2);
+            for (let i = 1; i < 20; i++) {
+                if (response2.meals[0]['strIngredient' + i] != null) {
+                    ing.push(response2.meals[0]['strIngredient' + i])
+                    meas.push(response2.meals[0]['strMeasure' + i])
+                    console.log(ing)
+                    console.log(meas)
+                }
+
+            }
+
+
 
         })
 
@@ -95,10 +98,9 @@ function ingredientSearch(ingredient) {
             slidesToShow: 3,
             slidesToScroll: 3,
             arrows: false,
-          });
+        });
 
     });
 
 }
-
 
