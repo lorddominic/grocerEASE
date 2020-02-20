9d7a7b412998a753e7572f1603a1492d313b23e1
Boyan Xu <developerxu123@gmail.com>
2020-02-20 14:19:56 -0700

    doms changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 let img = $('<img>')
 let ingredient
 let queryUrl = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" + ingredient
@@ -7,115 +5,79 @@ let searchIcon = $('#searchButton')
 let $div = ('<div>')
 var ing = []
 var meas = []
+var searchHistory = [];
+
+function goBack() {
+    ingredientSearch("Holycow");
+}
+
+function getHistory() {
+    $(".historyli").empty();
+    var parseHistory = JSON.parse(localStorage.getItem("history"));
+    parseHistory.forEach(element => {
+        var li = $("<li>").text(element)
+        $(".historyli").append(li);
+    })
+}
 
 $('.multiple-items').on('click', function(e) {
-
-    
     alert(this.attr())
 })
 
-$("#recipe-form").on("submit", function (event) {
+$("#recipe-form").on("submit", function(event) {
     event.preventDefault();
     console.log("search enter hit")
-
     let ingredient = $("#search-input").val().trim();
     console.log(ingredient);
-    
-
     ingredientSearch(ingredient);
-
-
 });
-
-searchIcon.on('click', function () {
-    let ingredient = $('.uk-search-input').val().trim();
-   
+searchIcon.on('click', function() {
+    let ingredient = $('#search-input').val().trim();
+    console.log(ingredient)
     ingredientSearch(ingredient);
-
+    event.preventDefault();
+    console.log(searchHistory);
+    searchHistory.push(ingredient);
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+    getHistory();
 });
-
 
 function ingredientSearch(ingredient) {
     console.log("ingredientSearch running");
     // let ingredient = $('.uk-search-input').val()
     let queryUrl = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" + ingredient
-    // console.log(ingredient)
+        // console.log(ingredient)
     $.ajax({
         method: "GET",
         url: queryUrl
-    }).then(function (response) {
-<<<<<<< HEAD
-        console.log(response)
-        if(!response.meals){
-            $('#error').html("I can't find that shit,trying something else!");
-            return;
-        }
-        $('#error').empty();
-        $(".cardContainer").empty();
-        for (let i = 0; i < response.meals.length; i++) {
-            
+    }).then(function(response) {
+        $('.multiple-items').empty()
+        for (let i = 0; i < 5; i++) {
             let meal = response.meals[i].strMeal
             let mealThumb = response.meals[i].strMealThumb
             let idMeal = response.meals[i].idMeal
-            let cardDiv = $("<div>").addClass('uk-card uk-card-default')
-            let mediaCard = $("<div>").addClass('uk-card-media-top')
-            let cardBody = $("<div>").addClass('uk-card-body')
-            let imageCard = $('<img>').attr('src', mealThumb)
-            let h3 = $("<h3>").text(meal)
-            $('.cardContainer').append(cardDiv)
-            cardDiv.append(mediaCard)
-            mediaCard.append(imageCard)
-            cardDiv.append(cardBody)
-            cardBody.append(h3)
-        }
-    
-    })
-};
-=======
+            let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal)
 
-
-
-        $('.multiple-items').empty()
-
-
-
-        for (let i = 0; i < 5; i++) {
-
-            
-           let meal = response.meals[i].strMeal
-           let mealThumb = response.meals[i].strMealThumb
-           let idMeal = response.meals[i].idMeal
-           let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal)
-    
             var sliderItem = $('<div>').append(imageCard)
-           $('.multiple-items').append(sliderItem)
-          
+            $('.multiple-items').append(sliderItem)
 
-           
-           
-
-           queryUrl2 = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
-           console.log(queryUrl2)
-
+            queryUrl2 = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
+            console.log(queryUrl2)
         }
-    
+
         $.ajax({
             method: "GET",
             url: queryUrl2
-        }).then(function (response2) {
-                console.log(response2);
-               for(let i = 1; i < 20; i++){
-               if(response2.meals[0]['strIngredient' + i] != null){
-                ing.push(response2.meals[0]['strIngredient' + i])
-                meas.push(response2.meals[0]['strMeasure' + i])
-                console.log(ing)
-                console.log(meas)
-               }
-                
-               }
-               
-            
-
+        }).then(function(response2) {
+            console.log(response2);
+            for (let i = 1; i < 20; i++) {
+                if (response2.meals[0]['strIngredient' + i] != null) {
+                    ing.push(response2.meals[0]['strIngredient' + i])
+                    meas.push(response2.meals[0]['strMeasure' + i])
+                    console.log(ing)
+                    console.log(meas)
+                }
+            }
         })
 
         $('.multiple-items').slick({
@@ -123,9 +85,6 @@ function ingredientSearch(ingredient) {
             slidesToShow: 3,
             slidesToScroll: 3,
             arrows: false,
-          });
-
+        });
     });
-
 }
->>>>>>> 5d2d44f1a5867aec43ce2abdc86696c45d6a3f76
