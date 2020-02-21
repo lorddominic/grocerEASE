136f73a77a99d95d2cfbae9dc3da62b92017ad26
Boyan Xu <developerxu123@gmail.com>
2020-02-21 09:31:07 -0700

    button

--- js/script.js
+++ js/script.js
@@ -7,11 +7,44 @@ var ing = []
 var meas = []
 var searchHistory = [];
 
-ingredientSearch(ingredient);
-event.preventDefault();
-console.log(searchHistory);
-searchHistory.push(ingredient);
-localStorage.setItem("history", JSON.stringify(searchHistory));
+function goBack() {
+    ingredientSearch("Holycow");
+}
+
+function getHistory() {
+    $(".historyli").empty();
+    var parseHistory = JSON.parse(localStorage.getItem("history"));
+    parseHistory.forEach(element => {
+        var li = $("<li>").text(element)
+        $(".historyli").append(li);
+    })
+}
+
+$('.multiple-items').on('click', function(e) {
+    alert(this.attr())
+})
+
+$("#recipe-form").on("submit", function(event) {
+    event.preventDefault();
+    console.log("search enter hit")
+    let ingredient = $("#search-input").val().trim();
+    console.log(ingredient);
+    ingredientSearch(ingredient);
+    event.preventDefault();
+    console.log(searchHistory);
+    searchHistory.push(ingredient);
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+    getHistory();
+});
+searchIcon.on('click', function() {
+    let ingredient = $('#search-input').val().trim();
+    console.log(ingredient)
+    ingredientSearch(ingredient);
+    event.preventDefault();
+    console.log(searchHistory);
+    searchHistory.push(ingredient);
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+    getHistory();
 });
 
 function ingredientSearch(ingredient) {
@@ -29,12 +62,6 @@ function ingredientSearch(ingredient) {
             let mealThumb = response.meals[i].strMealThumb
             let idMeal = response.meals[i].idMeal
             let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal)
-
-
-            let meal = response.meals[i].strMeal
-            let mealThumb = response.meals[i].strMealThumb
-            let idMeal = response.meals[i].idMeal
-            let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal).addClass('recipeImage')
 
             var sliderItem = $('<div>').append(imageCard)
             $('.multiple-items').append(sliderItem)
