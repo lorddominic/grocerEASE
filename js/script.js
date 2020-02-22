348a29c201769f29372bc40fea018e7754bf463f
Dane Burns <dane@lizaburnsandassociates.com>
2020-02-22 07:43:46 -0700

    algo work

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ var recipes = []
 var searchHistory = [];
 var ing = []
 var meas = []
-var ingmeas = [] 
-$("#home").on('click', function() {
+var ingmeas = []
+var newMeas = []
+
+$("#home").on('click', function () {
     ingredientSearch("holyshit");
 })
 
@@ -24,79 +26,98 @@ function getHistory() {
 }
 
 $('#generateShop').on('click', function (e) {
-
     $('.ingredientsContainer').append("<h3 class='uk-text-center uk-padding'>" + 'Your combined shopping list:' + '</h3>' + '<hr>')
+    for (let i = 0; i < recipes.length; i++) {
+        var ingredientsUrl = "https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=" + recipes[i]
+        console.log(ingredientsUrl)
 
-for (let i = 0; i < recipes.length; i++) {
-   
-    
+        $.ajax({
+            method: "GET",
+            url: ingredientsUrl
+        }).then(function (response2) {
+            for (let i = 1; i < 20; i++) {
+                if (response2.meals[0]['strIngredient' + i] != '') {
+                    ing.push(response2.meals[0]['strIngredient' + i])
+                    meas.push(response2.meals[0]['strMeasure' + i])
+                    ingmeas.push([response2.meals[0]['strIngredient' + i], response2.meals[0]['strMeasure' + i]])
+                    ingmeas = ingmeas.sort()
+                    
 
-   
-    var ingredientsUrl = "https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=" + recipes[i]
-    console.log(ingredientsUrl)
 
-    $.ajax({
-        method: "GET",
-        url: ingredientsUrl
-    }).then(function(response2) {
-        for (let i = 1; i < 20; i++) {
-        if (response2.meals[0]['strIngredient' + i] != '') {
-        
-        
-        
-        
-        ing.push(response2.meals[0]['strIngredient' + i])
-        meas.push(response2.meals[0]['strMeasure' + i])
-        
-        
-        ingmeas.push([response2.meals[0]['strIngredient' + i], response2.meals[0]['strMeasure' + i]])
-        var ingtest = ingmeas.sort()
-        console.log(ingtest)
-        // console.log(ingmeas)
-        // var shoots = $('<p>').text(ingmeas)
-        // $('.ingredientsContainer').append(shoots)
+                }
+               
+               
+                
             }
+            
+    
+        })
+    }
+ 
+
+  
+})
+
+
+
+
+function stuffToDo(ingmeas) {
+    console.log(newMeas)
+    if (ingmeas.length > 0) {
+        console.log(ingmeas)
+        if ((ingmeas[0][0]) === (ingmeas[1][0])) {
+            ingmeas[0].push(ingmeas[1][1])
+            newMeas.push(ingmeas[0])
+            
+            ingmeas.splice(0)
+            ingmeas.splice(1)
+             console.log('nuggets')
         }
-     
-       console.log(ingmeas)
 
-        
-    })
-    
+        else {
+            console.log('farts')
+            newMeas.push(ingmeas[0])
+            newMeas.push(ingmeas[1])
+            ingmeas.splice(0)
+            ingmeas.splice(1)
+        }
+    }
 }
+
+
+
+
+
+
+
+$('.multiple-items').on('click', function (e) {
+
+
+    recipes.push(($(event.target).attr('data-id')))
+
+
+    //   $(event.target).addClass('uk-invisible')
+    $('.underTitle').append(($(event.target)).parent())
+    //   $('.underTitle').append(($(event.target)))
+
+    $('.underTitle').children().attr('style', '')
+    $('.underTitle').children().addClass('fart')
+
+
+
+
 })
 
-
-
-
-$('.multiple-items').on('click', function(e) {
-    
-    
-  recipes.push(($(event.target).attr('data-id')))
-  
-  
-//   $(event.target).addClass('uk-invisible')
-  $('.underTitle').append(($(event.target)).parent())
-//   $('.underTitle').append(($(event.target)))
-  
-  $('.underTitle').children().attr('style', '')
-  $('.underTitle').children().addClass('fart')
-
-
-
-
-})
-
-$("#recipe-form").on("submit", function(event) {
+$("#recipe-form").on("submit", function (event) {
     event.preventDefault();
-    
+
 
     let ingredient = $("#search-input").val().trim();
-    
+
 
     ingredientSearch(ingredient);
     event.preventDefault();
-    
+
     searchHistory.push(ingredient);
     localStorage.setItem("history", JSON.stringify(searchHistory));
     getHistory();
@@ -109,50 +130,40 @@ searchIcon.on('click', function () {
 
     ingredientSearch(ingredient);
     event.preventDefault();
-   
+
     searchHistory.push(ingredient);
     localStorage.setItem("history", JSON.stringify(searchHistory));
     getHistory();
 });
 
 function ingredientSearch(ingredient) {
-   
+
     // let ingredient = $('.uk-search-input').val()
     let queryUrl = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" + ingredient
-       
+
     $.ajax({
         method: "GET",
         url: queryUrl
-    }).then(function(response) {
+    }).then(function (response) {
         $('.multiple-items').empty()
         for (let i = 0; i < 5; i++) {
-    
 
 
-            
-           let meal = response.meals[i].strMeal
-           let mealThumb = response.meals[i].strMealThumb
-           let idMeal = response.meals[i].idMeal
-           let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal).addClass('recipeImage')
-    
+
+
+            let meal = response.meals[i].strMeal
+            let mealThumb = response.meals[i].strMealThumb
+            let idMeal = response.meals[i].idMeal
+            let imageCard = $('<img>').attr('src', mealThumb).attr('data-id', idMeal).addClass('recipeImage')
+
             var sliderItem = $('<div>').append(imageCard)
-            var sliderText= sliderItem.append('<p>' + meal + '</p>')
+            var sliderText = sliderItem.append('<p>' + meal + '</p>')
             sliderText.addClass('uk-text-center uk-text-bold uk-text-large')
-           $('.multiple-items').append(sliderItem)
-           
+            $('.multiple-items').append(sliderItem)
 
-
-
-
-            
-           
-
-
-            
-            
         }
 
-    
+
 
         $('.multiple-items').slick({
             infinite: true,
